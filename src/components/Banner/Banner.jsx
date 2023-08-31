64e4f81b7dccca61d5a995dadd17122d27cd6e0c
musense <musense.ad@gmail.com>
2023-08-31 16:55:54 +0800

    banner OK (靜態)

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
 export default function Banner() {
-    return <div>Banner</div>;
+    return <div className="index-banner">
+        <div className="banner-icon prev" />
+        <div className="banner-icon next" />
+    </div>;
 }
