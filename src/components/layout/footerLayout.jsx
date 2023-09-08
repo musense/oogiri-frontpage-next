2f30a59b4a38716e1590101bf5af280eb30a80d5
musense <musense.ad@gmail.com>
2023-09-08 14:43:54 +0800

    首頁footer OK

--- src/components/layout/footerLayout.jsx
+++ src/components/layout/footerLayout.jsx
@@ -1,27 +1,28 @@
 import Tag from '@components/content/tag';
 import CommunityIcon from '@components/CommunityIcon/CommunityIcon';
+import styles from './css/footerLayout.module.css'
 
 export default function FooterLayout({ popularTagList, pathname }) {
-  // console.log("🚀 ~ file: footerLayout.jsx:6 ~ FooterLayout ~ pathname:", pathname)
+  console.log("🚀 ~ file: footerLayout.jsx:6 ~ FooterLayout ~ pathname:", pathname)
 
   pathname
-  const footerClass = pathname === ""
+  const footerClass = pathname === "" || pathname === "/"
     ? 'index-footer'
     : pathname.indexOf('/p') !== -1
       ? 'content-footer'
       : 'tag-footer'
 
   return (
-    <footer className={`footer-wrapper ${footerClass}`}>
-      <div className='footer-box'>
-        <div className='top-side'>
+    <footer className={`${styles['footer-wrapper']} ${styles[footerClass]}`}>
+      <div className={styles['footer-box']}>
+        <div className={styles['top-side']}>
           {/* <CommunityIcon type={'twitter'} />
           <CommunityIcon type={'blog'} /> */}
           <CommunityIcon type={'trend-tags'} />
           {/* <CommunityIcon type={'facebook'} />
           <CommunityIcon type={'instagram'} /> */}
         </div>
-        <div className='bottom-side'>
+        <div className={styles['bottom-side']}>
           {popularTagList && popularTagList.length > 0 && popularTagList.map((tag, index) => {
             return <Tag
               key={index}
@@ -31,6 +32,9 @@ export default function FooterLayout({ popularTagList, pathname }) {
             />;
           })}
         </div>
+      </div>
+      <div className={styles['footer-copyright']}>
+        <span>©2023 OOGIRI-info All rights reserved</span>
       </div>
     </footer>
   );
