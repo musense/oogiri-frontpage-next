a706858dc0e3c84737af70070859e4a89f2f2c1d
musense <musense.ad@gmail.com>
2023-10-03 13:12:42 +0800

    修復 「記事一覽」  文章列表 因為標籤文字太多突出問題

--- src/components/content/tag.jsx
+++ src/components/content/tag.jsx
@@ -2,12 +2,20 @@ import Link from "next/link";
 import React from "react";
 import styles from "./css/tag.module.css"
 
-export default function Tag({ href = '', tagName, icon = false, linkOn = true, footerLayout = false }) {
+export default function Tag({
+    href = '',
+    tagName,
+    icon = false,
+    linkOn = true,
+    footerLayout = false,
+    className = ''
+}) {
 
     const content = <span className={`
     ${styles['tag']} 
     ${icon ? styles['icon-tag'] : ''}
     ${footerLayout ? styles['footer-layout'] : ''}
+    ${className && styles[className]}
     `} >{tagName}</span>
     return linkOn ? (
         <Link className="tag-link" style={{ lineHeight: '1' }} href={href}>
